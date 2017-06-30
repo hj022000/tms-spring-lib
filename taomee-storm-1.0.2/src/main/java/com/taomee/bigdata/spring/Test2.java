1d27b4d7cbed0f320fa3731ff943385c2edeed44
hj022000 <2500833119@qq.com>
2017-06-30 11:47:22 +0800

    修复bug

---
+++ taomee-storm-1.0.2/src/main/java/com/taomee/bigdata/spring/Test2.java
@@ -0,0 +1,12 @@
+package com.taomee.bigdata.spring;
+
+/**
+ * Author looper.
+ * Company  TaoMee.Inc, ShangHai.
+ * Date  2017/6/30.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("修复bug");
+    }
+}
